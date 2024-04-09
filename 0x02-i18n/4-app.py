b6fb10c5b670804716dfea1802a9d7b76f23055a
Simamnkele <smambo85@gmail.com>
2024-04-09 18:35:24 +0200

    Changed app route

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -36,7 +36,7 @@ def get_locale() -> str:
 @app.route('/')
 def get_index() -> str:
     """Function returns the index page"""
-    return (render_template('3-index.html'))
+    return (render_template('4-index.html'))
 
 
 if __name__ == '__main__':
