9e35c4027d4f4ed9f8f4a69504c49f368564c6e3
Simamnkele <smambo85@gmail.com>
2024-05-07 17:23:37 +0000

    Script that connects to Redis server

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,12 @@
+// Connects to Redis server running locally
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
