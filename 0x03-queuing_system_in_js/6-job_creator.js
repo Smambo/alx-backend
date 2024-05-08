ac9d6f1f15beb8bb43a80a2161c3cb13c680b9f8
Simamnkele <smambo85@gmail.com>
2024-05-08 09:23:28 +0000

    Created the job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,21 @@
+// Job creator
+import { createQueue } from 'kue';
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '0123456789',
+  message: 'Account registered successfully',
+});
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
+job.save();
